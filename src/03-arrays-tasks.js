12298252ddb1955a3934982cb3e97db1cd2a0a1a
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-06-13 20:02:16 +0200

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -476,8 +476,9 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const uniq = new Set(arr);
+  return Array.from(uniq);
 }
 
 /**
