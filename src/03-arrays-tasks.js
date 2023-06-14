36177f2bf9a0e6f12a841d61585ec1dc321a08d6
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-06-15 00:25:41 +0200

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,12 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const array = Array(len)
+    .fill(null)
+    .map((_, index) => 2 * index + 1);
+
+  return array;
 }
 
 
