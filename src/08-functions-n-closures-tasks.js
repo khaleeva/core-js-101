ddd4a81086f4a9727709374af6663fe2a33a3b33
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-06-23 00:46:32 +0200

    feat: add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -85,8 +85,14 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  return function () {
+    if (cache === null) {
+      cache = func();
+    }
+    return cache;
+  };
 }
 
 
